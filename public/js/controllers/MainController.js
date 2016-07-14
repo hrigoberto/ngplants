41d8342ce1829e51a47dd9b3a7ece5a1ad699006
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-07-13 20:52:49 -0400

    added create and delete functions

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -6,15 +6,36 @@
 
   function MainController($scope, PlantService){
     $scope.plants = PlantService.plants;
+    $scope.create = createPlant;
+    $scope.delete = deletePlant;
     getPlants();
 
 
-  function getPlants(){
-    PlantService.readAll()
-                .then(function(){
-                  plants = PlantService.plants
-                  console.log(plants);
-                });
+    function getPlants(){
+      PlantService.readAll()
+                  .then(function(){
+                    $scope.plants = PlantService.plants
+                    console.log($scope.plants);
+                  });
+      }
+
+    function createPlant(commonName,scientificName,layerType,use){
+      PlantService.create(commonName,scientificName,layerType,use)
+                  .then(function(){
+                    $scope.commonName = '',
+                    $scope.scientificName = '',
+                    $scope.layerType = '',
+                    $scope.use = '',
+                    getPlants();
+                  });
+    }
+
+    function deletePlant(id){
+      console.log(id);
+      PlantService.delete(id)
+                  .then(function(){
+                    getPlants();
+                  });
     }
 
   }
